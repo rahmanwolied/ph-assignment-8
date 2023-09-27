7b1d99ef02078a61a82004d90cc12663678135b2
rahmanwolied <mosheur.r.wolied@gmail.com>
2023-09-27 23:50:58 +0600

    final changes

--- src/components/NotFound.jsx
+++ src/components/NotFound.jsx
@@ -4,7 +4,7 @@ function NotFound() {
 			<div className="m-auto text-center">
 				<h1 className="text-5xl font-semibold text-gray-800 mb-4">404</h1>
 				<p className="text-xl text-gray-600 mb-4">Oops! Page not found.</p>
-				<p className="text-gray-500 mb-8">The page you are looking for might have been removed or doesn't exist.</p>
+				<p className="text-gray-500 mb-8">The page you are looking for might have been removed or does not exist.</p>
 				<button
 					className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded-full focus:outline-none focus:shadow-outline-blue active:bg-blue-800"
 					onClick={() => {
